7b9dde787d53568992174d8bd64d84d3be9fd671
Mostafa Ayyad <mostafaayyad33@gmail.com>
2022-01-22 02:10:00 +0200

    Adding the final complete code

--- stats.c
+++ stats.c
@@ -51,46 +51,128 @@ void main() {
 
 // Function that prints an array
 void print_array(unsigned char x[], int s){
-  
+  int c;
+  for(c = 0; c < s; c++){
+    printf("%d ", x[c]);
+  }
+  printf("\n");
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Function that prints statistics of an array. (Contains max, min, mean and median function calls)
 void print_statistics(){
-  
+  float med = find_median(test, SIZE);
+  printf("The median is: %0.1f \n", med);
+
+  float mean = find_mean(test, SIZE);
+  printf("The mean is: %0.1f \n", mean);
+
+  int max = find_maximum(test, SIZE);
+  printf("The max value is: %d \n", max);
+
+  int min = find_minimum(test, SIZE);
+  printf("The max value is: %d \n", min);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Function that calculate the median of an array
 float find_median(unsigned char x[], int s){
-  
+  int c,m, hold;
+  float med;
+
+  //sorting the array first (greatest value have an index of 0)
+  for(m=0; m<s; m++){
+      for(c=m; c<s; c++){
+          if(x[c] > x[m]){
+              hold = x[m];
+              x[m] = x[c];
+              x[c] = hold;
+          }
+      }
+  }
+
+  if (s%2 == 0){
+    med = (x[s/2] + x[s/2 -1]) /2.0;
+  }
+  else{
+    med = x[s/2];
+  }
+
+  return med;
+
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Function that calculate the mean of an array
 float find_mean(unsigned char x[], int s){
-  
+  int c, sum=0;
+  float mean;
+  for(c = 0; c < s; c++){
+    sum += x[c];
+  }
+  mean = (float)sum/s;
+  return mean;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Function that outputs the max number in an array
 int find_maximum(unsigned char x[], int s){
-  
+  int c,m, hold, max;
+
+  //sorting the array first (greatest value have an index of 0)
+  for(m=0; m<s; m++){
+      for(c=m; c<s; c++){
+          if(x[c] > x[m]){
+              hold = x[m];
+              x[m] = x[c];
+              x[c] = hold;
+          }
+      }
+  }
+
+  max = x[0];
+
+  return max;
+
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Function that outputs the min number in an array
 int find_minimum(unsigned char x[], int s){
-  
+  int c,m, hold, min;
+
+  //sorting the array first (greatest value have an index of 0)
+  for(m=0; m<s; m++){
+      for(c=m; c<s; c++){
+          if(x[c] > x[m]){
+              hold = x[m];
+              x[m] = x[c];
+              x[c] = hold;
+          }
+      }
+  }
+
+  min = x[s-1];
+
+  return min;
+
 }
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 //Function that sorts an array (greatest value have an index of 0)
 void sort_array(unsigned char x[], int s){
-  
+  int c, m, hold;
+  for(m=0; m<s; m++){
+      for(c=m; c<s; c++){
+          if(x[c] > x[m]){
+              hold = x[m];
+              x[m] = x[c];
+              x[c] = hold;
+          }
+      }
+  }
 }
-
 
 
 
